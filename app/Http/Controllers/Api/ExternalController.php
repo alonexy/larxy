34d68c00941b6a871c7b481c8ef31f541856ca9a
alonexy <hpl1124@126.com>
2018-10-12 13:43:55 +0800

    update

--- app/Http/Controllers/Api/ExternalController.php
+++ app/Http/Controllers/Api/ExternalController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use Alonexy\Helpers\HpFuns;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -18,5 +19,7 @@ class ExternalController extends Controller
         dump($result);
         $dd = $calendar->lunar(2018,$result['lunar_month'], $result['lunar_day']);
         dump($dd);
+        $helper = new HpFuns();
+        dd($helper->getCycleTime('2018-09-01','2018-08-01',3));
     }
 }
